4360ed7c9fc1f338ba31250e130e1deadbe0c7f6
Serikzhan <kuanyshev_2020@mail.ru>
2022-06-01 01:02:12 +0600

    closed prepared statements

--- src/dao/UserDaoImpl.java
+++ src/dao/UserDaoImpl.java
@@ -21,54 +21,53 @@ public class UserDaoImpl implements UserDao {
     @Override
     public boolean add(User user) throws SQLException {
         String sql = "INSERT INTO users(id, name) VALUES (?, ?)";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1, String.valueOf(user.getId()));
-        preparedStatement.setString(2, user.getName());
-        boolean execute = preparedStatement.execute();
-        preparedStatement.close();
-        return execute;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, String.valueOf(user.getId()));
+            preparedStatement.setString(2, user.getName());
+            return preparedStatement.execute();
+        }
     }
 
     @Override
     public void delete(Long id) throws SQLException {
         String sql = "delete from users where id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1, String.valueOf(id));
-        preparedStatement.execute();
-        preparedStatement.close();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, String.valueOf(id));
+            preparedStatement.execute();
+        }
     }
 
     @Override
     public User getUser(Long id) throws SQLException {
         String sql = "SELECT * FROM users where id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1, String.valueOf(id));
-        ResultSet resultSet = preparedStatement.executeQuery();
-        preparedStatement.close();
-        return parseResultSetToUser(resultSet);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, String.valueOf(id));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return parseResultSetToUser(resultSet);
+        }
     }
 
     @Override
     public List<User> getUsers() throws SQLException {
         String sql = "SELECT * FROM users";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        List<User> users = new ArrayList<>();
-        while (resultSet.next()) {
-            users.add(parseResultSetToUser(resultSet));
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<User> users = new ArrayList<>();
+            while (resultSet.next()) {
+                users.add(parseResultSetToUser(resultSet));
+            }
+            return users;
         }
-        preparedStatement.close();
-        return users;
     }
 
     @Override
     public void update(User user) throws SQLException {
         String sql = "UPDATE users SET name = ? WHERE id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, String.valueOf(user.getId()));
-        preparedStatement.execute();
-        preparedStatement.close();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, String.valueOf(user.getId()));
+            preparedStatement.execute();
+        }
     }
 
     public User parseResultSetToUser(ResultSet resultSet) throws SQLException {
